bbf298478971e11e506434ac991412eefdece8f4
Anmol Gautam <tarptaeya@gmail.com>
2022-03-16 19:01:26 +0530

    cached api calls

--- repo-card.js
+++ repo-card.js
@@ -1,7 +1,14 @@
 window.addEventListener('DOMContentLoaded', async function() {
   async function get(url) {
+    const now = new Date().getTime();
+    const prevResp = JSON.parse(localStorage.getItem(url));
+    if (prevResp && Math.abs(now - prevResp.time) < 60000) {
+      return prevResp.data;
+    }
     const resp = await fetch(url);
-    return resp.json();
+    const json = await resp.json();
+    localStorage.setItem(url, JSON.stringify({time: now, data: json}));
+    return json;
   }
 
   const emojis = await get('https://api.github.com/emojis');
@@ -22,7 +29,7 @@ window.addEventListener('DOMContentLoaded', async function() {
     }
   };
 
-  document.querySelectorAll('.repo-card').forEach(async function(el) {
+  for (const el of document.querySelectorAll('.repo-card')) {
     const name = el.getAttribute('data-repo');
     const theme = themes[el.getAttribute('data-theme') || 'light-default'];
     const data = await get(`https://api.github.com/repos/${name}`);
@@ -32,7 +39,7 @@ window.addEventListener('DOMContentLoaded', async function() {
       const emoji = emojis[name];
 
       if (emoji) {
-        return `<span><img src="${emoji}" style="width: 1rem; height: 1rem; vertical-align: -0.2rem;"></span>`;
+        return `<span><img src="${emoji}" style="width: 1rem; height: 1rem; vertical-align: -0.2rem;" alt="${name}"></span>`;
       }
 
       return match;
@@ -53,16 +60,16 @@ window.addEventListener('DOMContentLoaded', async function() {
           <span style="width: 12px; height: 12px; border-radius: 100%; background-color: ${data.language ? colors[data.language].color : ''}; display: inline-block; top: 1px; position: relative;"></span>
           <span>${data.language}</span>
         </div>
-        <div style="display: ${data.stargazers_count == 0 ? 'none' : 'flex'}; align-items: center; margin-right: 16px;">
+        <div style="display: ${data.stargazers_count === 0 ? 'none' : 'flex'}; align-items: center; margin-right: 16px;">
           <svg style="fill: ${theme.color};" aria-label="stars" viewBox="0 0 16 16" version="1.1" width="16" height="16" role="img"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg>
           &nbsp; <span>${data.stargazers_count}</span>
         </div>
-        <div style="display: ${data.forks == 0 ? 'none' : 'flex'}; align-items: center;">
+        <div style="display: ${data.forks === 0 ? 'none' : 'flex'}; align-items: center;">
           <svg style="fill: ${theme.color};" aria-label="fork" viewBox="0 0 16 16" version="1.1" width="16" height="16" role="img"><path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path></svg>
           &nbsp; <span>${data.forks}</span>
         </div>
       </div>
     </div>
     `;
-  });
+  }
 });
